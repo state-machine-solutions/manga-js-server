e8064e5d97d16fd0747d81fe8576b83b21d25d04
Renato <reytuty@gmail.com>
2024-08-20 17:53:40 -0300

    chore: fix raw data response

--- modules/server/modules/HttpServer.js
+++ modules/server/modules/HttpServer.js
@@ -44,7 +44,7 @@ function HttpServer(stateMachineServer, config = null) {
         console.log('HTTP get : ' + req.query?.path);
         stateMachineServer.get(req.query.path).then((data) => {
             res.setHeader('Content-Type', 'application/json');
-            res.status(200).send(data);
+            res.status(200).send(JSON.stringify(data));
         });
     };
 
